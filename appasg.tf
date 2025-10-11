8f8bf379e04da6d62fd3c09ec5677b9c4fbf7a67
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2025-10-11 09:24:43 +0530

    Update appasg.tf

--- appasg.tf
+++ appasg.tf
@@ -20,7 +20,7 @@ resource "aws_autoscaling_group" "swiggy-app-asg" {
 # Create a launch template for the EC2 instances
 resource "aws_launch_template" "swiggy-app-template" {
   name_prefix   = "swiggy-app-template"
-  image_id      = "ami-0b982602dbb32c5bd"
+  image_id      = "ami-0f9708d1cd2cfee41"
   instance_type = "t3.micro"
   key_name      = "3tierproject"
 
