569855a627c742b48b0dc7c1377eb4a20917b624
devopsbyraham <125343344+devopsbyraham@users.noreply.github.com>
2025-10-11 09:25:27 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor4pmbatch-demko"
+  bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor9ambatch-demko"
 
   # Enable versioning
   versioning {
