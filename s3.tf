c47d1414307c9ac4e800098a85346fdaf7f338b9
Onkar <onkar19supate@gmail.com>
2025-10-23 21:28:34 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,11 @@
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform-fordemoinfra3tirer4567for9ambatch-demko"
 
-  # Enable versioning
-  versioning {
-    enabled = false
+  resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
+  bucket = aws_s3_bucket.example_bucket.id # Reference the bucket by its ID
+
+  versioning_configuration {
+    status = "active" # Use `status` instead of `enabled`
   }
 }
 
