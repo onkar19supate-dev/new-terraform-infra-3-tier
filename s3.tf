cb11f656a751a7bf84f334fa5faec3e086d86fa4
Onkar <onkar19supate@gmail.com>
2025-10-23 20:41:14 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,6 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
-  bucket = "swiggy-bucket-terraform-fordemoinfra3tirerrrrfor9ambatch-demko"
+  bucket = "swiggy-bucket-terraform-fordemoinfra3tirer4567for9ambatch-demko"
 
   # Enable versioning
   versioning {
