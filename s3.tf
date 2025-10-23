a93190b2573208bf7baf4b732df94e72b9c77c88
Onkar <onkar19supate@gmail.com>
2025-10-23 21:31:16 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,6 +1,7 @@
 # Create an S3 Bucket
 resource "aws_s3_bucket" "example_bucket" {
   bucket = "swiggy-bucket-terraform-fordemoinfra3tirer4567for9ambatch-demko"
+}
 
   resource "aws_s3_bucket_versioning" "example_bucket_versioning" {
   bucket = aws_s3_bucket.example_bucket.id # Reference the bucket by its ID
